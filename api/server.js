a67d3b1f1efb79b28e165f0c5338a11c1061cf56
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-27 12:32:03 -0700

    Set up directories, added dependencies, set up routes

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authRouter = require("../auth/auth-router");
+
+const restricted = require("../auth/restricted-middleware");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, usersRouter);
+// server.use("/api/recipes", restricted, recipeRouter);
+
+server.get("/", (req, res) => {
+  res.send({ message: "It's alive!" });
+});
+
+module.exports = server;
---
+++ posts/posts-router.js
@@ -0,0 +1,100 @@
+const router = require("express").Router();
+
+const Posts = require("../posts/posts-model");
+
+//for endpoints beginning with /api/posts
+
+router.get("/", (req, res) => {
+  //return all posts
+  Posts.find()
+    .then((post) => {
+      res.status(200).json(recipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Cannot get posts at this time" });
+    });
+});
+
+router.get("/user/:id", (req, res) => {
+  //returns all posts for the user by their id
+  const { id } = req.params;
+
+  Recipes.findPostByUserId(id)
+    .then((post) => {
+      post
+        ? res.status(200).json(post)
+        : res.status(404).json({ error: "that user does not have any posts" });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "cannot find posts by that user at this time" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  //returns posts by post id
+  const { id } = req.params;
+
+  Recipes.findById(id)
+    .then((post) => {
+      post
+        ? res.status(200).json(post)
+        : res.status(404).json({ error: "cannot find post with that id" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "cannot get post at this time" });
+    });
+});
+
+router.post("/user/:id", (req, res) => {
+  //adds a new post to the users post list by user id
+  const newPost = req.body;
+  const { id } = req.params;
+
+  Posts.add(newPost, id)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "cannot add post at this time" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  //edits a post by post id
+  const { id } = req.params;
+  const changes = req.body;
+
+  Posts.findById(id)
+    .then((post) => {
+      post
+        ? Posts.update(changes, id).then((changes) => {
+            res.status(200).json(changes);
+          })
+        : res.status(404).json({ error: "no post with that id" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "cannot edit post at this time" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  //deletes a post by id
+  const { id } = req.params;
+  Posts.remove(id)
+    .then((deleted) => {
+      deleted
+        ? res.status(200).json(deleted)
+        : res.status(404).json({ error: "cannot find post with id" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "cannot delete post" });
+    });
+});
+
+module.exports = router;
