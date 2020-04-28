7a8dbca4791cf06c87133de5be19ea289bf3586c
Sandra Coburn <sandra_coburn@hotmail.com>
2020-04-27 20:04:03 -0700

    Updated database tables, users and posts

--- data/migrations/20200427194737_create-posts-table.js
+++ data/migrations/20200427194737_create-posts-table.js
@@ -2,9 +2,9 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("users", (tbl) => {
       tbl.increments();
-      tbl.string("username", 255).notNullable().unique();
-      tbl.string("password", 255).notNullable();
-      tbl.string("email", 255).notNullable();
+      tbl.string("username", 48).notNullable().unique();
+      tbl.string("password", 48).notNullable();
+      tbl.string("email", 48).notNullable();
     })
     .createTable("posts", (tbl) => {
       tbl.increments();
